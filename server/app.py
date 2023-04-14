dc512379b0b8a449bf239ad7d407e7da56d83770
Grace Nieboer <grace.nieboer@outlook.com>
2023-04-14 11:42:58 -0400

    page view tracker using cookies complete

--- server/app.py
+++ server/app.py
@@ -22,13 +22,20 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = [a.to_dict() for a in Article.query.all()]
+    return make_response(jsonify(articles), 200)
 
 @app.route('/articles/<int:id>')
 def show_article(id):
 
-    pass
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
+
+    if session['page_views'] <= 3:
+        article = Article.query.filter_by(id=id).first()
+        return make_response(jsonify(article.to_dict()))
+    
+    return {'message': 'Maximum pageview limit reached'}, 401
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5555, debug=True)
